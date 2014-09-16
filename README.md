f68e1546c68425b03c757aeb2cb088c8c2dbc20b
Tibor Udvari <tibor.udvari@icloud.com>
2014-09-16 12:42:07 +0000

    Retrieving basic profile info

---
+++ README.md
@@ -0,0 +1,5 @@
+LinkedIn Demo App - Filling out a form with the js API
+======================================================
+
+1. Create an app at developer.linkedin.com
+2. Add javascript API domains using the web interface
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+function onLinkedInAuth() {
+  IN.API.Profile("me")
+    .result( function(me) {
+      member = me.values[0];
+
+      console.log(member);
+	
+	document.getElementById("ln-name").innerHTML = member.firstName + ' ' +  member.lastName;
+    document.getElementById("ln-header").innerHTML = member.headline;
+    document.getElementById("ln-picture").src = member.pictureUrl;
+    });
+}
+
+
